06f6237adf268e65bb51151d5ba152e90342fde6
Daniil Agarkov <DAgarkov@corp.mm-software.com>
2026-04-30 10:35:12 +0200

    vite config fix

--- project/vite-project/vite.config.js
+++ project/vite-project/vite.config.js
@@ -5,9 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        allowedHosts:[
+                'jetson-va.local',
+                '192.168.3.23'
+        ],
+      hmr: {
+        clientPort: 443,
+        protocol: 'wss',
+        host: 'jetson-va.local',
+        },
     proxy: {
       '/api': 'http://127.0.0.1:8000',
       '/health': 'http://127.0.0.1:8000',
+
     },
   },
 })
